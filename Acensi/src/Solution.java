77dfe2d6e6b042a2ef7a18d3a320e2fe2ad18928
I327247 <I327247@PARD33959941A.emea.global.corp.sap>
2016-11-18 16:16:46 +0100

    2nd game implemented

--- Acensi/src/Solution.java
+++ Acensi/src/Solution.java
@@ -1,70 +1,148 @@
+import java.awt.Point;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Solution {
 	
-	public static String COLOR;
-	public static int N = 29;
-	public static int NB_TURNS = 40;
+	static boolean ACENSI = false;
 	
-	public static boolean ACENSI = true;
+	static int R;
+	static int C;
+	static int N = 3;
+	static boolean EXIT_FOUND = false;
+	static Point EXIT_POINT = null;
+	static ArrayList<Direction> moves = new ArrayList<Direction>();
+	static Direction[] directions = {Direction.DOWN, Direction.UP, Direction.LEFT, Direction.RIGHT};
+	
+	public enum Direction {
+		UP (-1, 0),
+		DOWN (1, 0),
+		LEFT (0, -1),
+		RIGHT (0, 1);
+		
+		private int deltaX, deltaY ;
+		
+		Direction (int x, int y) {
+			this.deltaX = x;
+			this.deltaY = y;
+		}
+		
+		public Point getDelta() {
+			return new Point(deltaX, deltaY);
+		}
+		
+		
+	}
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		
-        NB_TURNS--;
-        
-        System.out.println("Pyramides 29 ->" + pyramide(29));
 		Scanner in = null;
 		if(ACENSI)
 			in = new Scanner(System.in);
 		else 
-			in = new Scanner(new File("bot.txt"));
+			in = new Scanner(new File("bot2.txt"));
+		
+		int id = in.nextInt();
 		
 		
-		COLOR = in.nextLine().trim();
+		Character[][] map = readMap(in);
+		List<Direction> possibleMoves = possibleMoves(map);
 		
-		Character[][] board = readBoard(in);
-		
-		int x = -1;
-		int y = -1;
-		do {
-			Random rand = new Random();
-			x = rand.nextInt(10);
-			y = rand.nextInt(10);
+		if (moves.size() > 2) {
+			System.out.println("YEAAAAAAAAAAAj");
+		}
+		if(EXIT_FOUND) {
+			if (EXIT_POINT.x > 1) {
+				if(possibleMoves.contains(Direction.DOWN)) {
+					move(Direction.DOWN);
+					return;
+				}
+			}
 			
-		}while(board[x][y] != '-');
-		
-		System.out.println(x+" "+y);
-		System.err.println(NB_TURNS);
-	}
-	
-	private static Character[][] readBoard(Scanner in) {
-		Character[][] board = new Character[N][N];
-		
-		for (int i=0; i<N; i++) {
-			String line = in.nextLine();
-			for (int j=0; j<N; j++) {
-				board[i][j] = line.charAt(j);
+			if (EXIT_POINT.x < 1) {
+				if(possibleMoves.contains(Direction.UP)) {
+					move(Direction.UP);
+					return;
+				}
+			}
+			
+			if (EXIT_POINT.y < 1) {
+				if(possibleMoves.contains(Direction.LEFT)) {
+					move(Direction.LEFT);
+					return;
+				}
+			}
+			
+			if (EXIT_POINT.y > 1) {
+				if(possibleMoves.contains(Direction.RIGHT)) {
+					move(Direction.RIGHT);
+					return;
+				}
 			}
 		}
-		return board;
-	}
-	
-	public static int pyramide(int a) {
-		int steps = 0;
-		
-		int left = a;
-		
-		while(left > steps) {
-			steps++;
-			left = left-steps;
+		else {
+			if(possibleMoves.contains(Direction.UP)) {
+				move(Direction.UP);
+				return;
+			}
+			else if (possibleMoves.contains(Direction.RIGHT)) {
+				move(Direction.RIGHT);
+				return;
+			}
+			else if (possibleMoves.contains(Direction.DOWN)) {
+				move(Direction.DOWN);
+				return;
+			}
+			else {
+				move(Direction.LEFT);
+				return;
+			}
 			
 		}
 		
-		return steps;
 	}
+	
+	private static Character[][] readMap(Scanner in) {
+		Character[][] map = new Character[N][N];
+		
+		for (int i=0; i<N; i++) {
+			String line = in.next();
+			
+			for (int j=0; j<N; j++) {
+				map[i][j] = line.charAt(j);
+				if(map[i][j] == 'e') {
+					EXIT_FOUND = true;
+					EXIT_POINT = new Point(i,j);
+				}
+			}
+		}
+		return map;
+	}
+	
+	private static List<Direction> possibleMoves(Character[][] map) {
+		List<Direction> possibleMoves = new ArrayList<Direction>();
+		
+		int xbot = 1;
+		int ybot = 1;
+		
+		for (Direction d : directions) {
+			Point delta = d.getDelta();
+			if (map[xbot+delta.x][ybot+delta.y] != '#')
+				possibleMoves.add(d);
+		}
+		
+		return possibleMoves;
+	}
+	
+	private static void move(Direction d) {
+		moves.add(d);
+		System.out.println(d);
+		return;
+	}
+	
 
 }
